8cda95b0c14e1bef196ef275c2283f0acb2ddd30
jubitjohn <jubitjohn1@gmail.com>
2021-05-05 17:32:37 +0530

    parcel table added

--- database/cms_db.sql
+++ database/cms_db.sql
@@ -1,8 +1,38 @@
---jubit
+--Parcel
 
---anushika
+CREATE TABLE `parcels` (
+  `id` int(30) NOT NULL,
+  `reference_number` varchar(100) NOT NULL,
+  `sender_name` text NOT NULL,
+  `sender_address` text NOT NULL,
+  `sender_contact` text NOT NULL,
+  `recipient_name` text NOT NULL,
+  `recipient_address` text NOT NULL,
+  `recipient_contact` text NOT NULL,
+  `type` int(1) NOT NULL COMMENT '1 = Deliver, 2=Pickup',
+  `from_branch_id` varchar(30) NOT NULL,
+  `to_branch_id` varchar(30) NOT NULL,
+  `weight` varchar(100) NOT NULL,
+  `height` varchar(100) NOT NULL,
+  `width` varchar(100) NOT NULL,
+  `length` varchar(100) NOT NULL,
+  `price` float NOT NULL,
+  `status` int(2) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---jouhar
+--
+-- Dumping data for table `parcels`
+--
 
---aakhil
+INSERT INTO `parcels` (`id`, `reference_number`, `sender_name`, `sender_address`, `sender_contact`, `recipient_name`, `recipient_address`, `recipient_contact`, `type`, `from_branch_id`, `to_branch_id`, `weight`, `height`, `width`, `length`, `price`, `status`, `date_created`) VALUES
+(4, '514912669061', 'Adrian Mercurio', 'Brgy. Su-ay Himamaylan City, Negros Occidental', '+123456', 'Adones Evangelista', 'Brgy. Tan-awan Kabankalan City, Negros Occidental', '+12345', 2, '4', '1', '23kg', '12in', '12in', '15in', 1900, 0, '2020-11-27 13:52:14'),
+(6, '505604168988', 'Adrian Mercurio', 'Brgy. Su-ay Himamaylan City, Negros Occidental', '+123456', 'angel jude suarez', 'Manila, Philippines', '+12345', 1, '1', '', '23kg', '12in', '12in', '15in', 2500, 1, '2020-11-27 14:06:42');
+
+
+
+
+
+
+
 
