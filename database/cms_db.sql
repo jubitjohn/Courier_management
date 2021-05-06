657ce153155671884335ab06fbf36167b42f498e
Skillan202 <jou202jul@gmail.com>
2021-05-06 13:28:30 +0530

    change

--- database/cms_db.sql
+++ database/cms_db.sql
@@ -1,39 +1,20 @@
--- Table structure for table `parcel_tracks`
---
+/*Anushika p j */
+
+
+/*JUBIT*/
+
+
+
+/*AAKHIL*/
+
+
+
+/*JOUHAR*/
 
 CREATE TABLE `parcel_tracks` (
   `id` int(30) NOT NULL,
   `parcel_id` int(30) NOT NULL,
-  `status` int(2) NOT NULL,
-  `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `parcel_status` text NOT NULL, 
+) 
 
---
--- Dumping data for table `parcel_tracks`
---
 
-INSERT INTO `parcel_tracks` (`id`, `parcel_id`, `status`, `date_created`) VALUES
-(1, 2, 1, '2020-11-27 09:53:27'),
-(2, 3, 1, '2020-11-27 09:55:17'),
-(3, 1, 1, '2020-11-27 10:28:01'),
-(4, 1, 2, '2020-11-27 10:28:10'),
-(5, 1, 3, '2020-11-27 10:28:16'),
-(6, 1, 4, '2020-11-27 11:05:03'),
-(7, 1, 5, '2020-11-27 11:05:17'),
-(8, 1, 7, '2020-11-27 11:05:26'),
-(9, 3, 2, '2020-11-27 11:05:41'),
-(10, 6, 1, '2020-11-27 14:06:57');
-
--- --------------------------------------------------------
--- Indexes for table `parcel_tracks`
---
-ALTER TABLE `parcel_tracks`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for table `parcel_tracks`
---
-ALTER TABLE `parcel_tracks`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
