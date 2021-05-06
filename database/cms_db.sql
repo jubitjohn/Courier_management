01d7f573a00972c9c708a578b511e243de5e9312
Anushikapj <anushika1647@gmail.com>
2021-05-06 22:47:27 +0530

    second commit

--- database/cms_db.sql
+++ database/cms_db.sql
@@ -1,6 +1,6 @@
 
 create table branches (
-    id           int(20) NOT NULL AUTO_INCREMENT,
+    ID          int(20) NOT NULL AUTO_INCREMENT,
     BranchID        int             not null,
     BranchName      varchar(255)    not null,
     District        varchar(50)     not null,
@@ -9,8 +9,8 @@ create table branches (
     Pin_code        varchar(10)     not null,
     PRIMARY KEY(id)
 );
-INSERT INTO `branches` (`id`, `BranchID`, `BranchName`, ` District `, ` City`, ` ContactNum `, ` Pin_code`)
- VALUE
+INSERT INTO `branches` (`ID`, `BranchID`, `BranchName`, ` District `, ` City`, ` ContactNum `, ` Pin_code`)
+ VALUES
  /*id  BranchID   BranchName     District        City      ContactNum         Pin_code */
   (1,  '10',     'firstbranch',  'Ernakulam',   'Kochi1',  '+91123456789',   '682020'),
   (3,  '20',     'secondbranch', 'Idukki',      'kochi2',  '+91098765432',   '682021'),
