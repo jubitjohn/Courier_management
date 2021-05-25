cf9656f7dc1df2c206153ef08d0a7291452e856f
Skillan202 <jou202jul@gmail.com>
2021-05-25 21:48:37 +0530

    j

--- insert.php
+++ insert.php
@@ -1,7 +1,24 @@
 <?php
-include_once('db_connect.php')
-?>
-<?php
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = 'root';
+$db_db = 'db_couriers';
+$db_port = 8889;
+
+$conn = mysqli_connect(
+    $db_host,
+    $db_user,
+    $db_password,
+    $db_db
+);
+
+if ($conn->connect_error) {
+    echo 'Errno: ' . $conn->connect_errno;
+    echo '<br>';
+    echo 'Error: ' . $conn->connect_error;
+    exit();
+}
+
 $branch_id = $_POST['branch_id'];
 $branch_name = $_POST['branch_name'];
 $district = $_POST['district'];
