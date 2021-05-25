b78b1b17a213f6654453d96c1fa92387f66a9334
Skillan202 <jou202jul@gmail.com>
2021-05-25 17:07:21 +0530

    change

--- insert.php
+++ insert.php
@@ -1,43 +1,27 @@
 <?php
-$server="localhost";
-$username="root";
-$password="";
-$dbname="cms_db";
-
-$conn = mysqli_connect($server,$username,$password,$dbname);
-if(isset($_POST['submit'])){
-    if(!empty($_POST['branch_id']) && !empty($_POST['branch_name']) && !empty($_POST['district']) &&! empty($_POST['city'])  && !empty($_POST['contact_number'])  && !empty($_POST['pincode']))
-    {
-        $branch_id =$_POST['branch_id'];
-        $branch_name =$_POST['branch_name'];
-        $district =$_POST['district'];
-        $city =$_POST['city'];
-        $contact_number =$_POST['contact_number'];
-        $pincode =$_POST['pincode'];
-        
-
-        $query="INSERT INTO Branches(BranchId,BranchName,District,City,ContactNum,Pin_code) VALUES('$branch_id','$branch_name','$district','$city','$contact_number','$pincode')";
-
-
-        $run= mysqli_query($conn,$query) ;
-        if($run){
-           echo "Form submitted successfully" ;
-        }
-
-        else{
-            echo "Form not submitted successfully" ;
-        }
-    }
-    else{
-        echo "All fields required" ;
-    }
-
-       
-
-
-
-        
-        
+$con = mysqli_connect('localhost', 'root', '');
+if (!$con) {
+    echo "Not connected To Server";
 }
+if (mysqli_select_db($con, 'cms_db')) {
+    echo 'Database not selected';
+}
+$branch_id = $_POST['branch_id'];
+$branch_name = $_POST['branch_name'];
+$district = $_POST['district'];
+$city = $_POST['city'];
+$contact_number = $_POST['contact_number'];
+$pincode = $_POST['pincode'];
+$sql = "INSERT INTO Branches(BranchId,BranchName,District,City,ContactNum,Pin_code) VALUES('$branch_id','$branch_name','$district','$city','$contact_number','$pincode')";
+
+if(mysqli_query($con,$sql))
+{
+    echo "Not Inserted";
+}
+else
+{
+    echo "Inserted";
+}
+
 
 ?>
