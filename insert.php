388c1eadebca17ffb91796550158b0c5ce21d3a7
Skillan202 <jou202jul@gmail.com>
2021-05-27 21:26:00 +0530

    j

--- insert.php
+++ insert.php
@@ -1,19 +1,11 @@
 <?php
-include_once ('db_connect.php') ;
-$branch_id = $_POST['branch_id'];
-$branch_name = $_POST['branch_name'];
-$district = $_POST['district'];
-$city = $_POST['city'];
-$contact_number = $_POST['contact_number'];
-$pincode = $_POST['pincode'];
-$sql = "INSERT INTO Branches(BranchId,BranchName,District,City,ContactNum,Pin_code) VALUES('$branch_id','$branch_name','$district','$city','$contact_number','$pincode')";
-
-if(mysqli_query($con,$sql))
-{
-    echo "Not Inserted";
-}
-else
-{
-    echo "Inserted";
-}
+    include_once ('db_connect.php') ;
+    $branch_id = $_POST['branch_id'];
+    $branch_name = $_POST['branch_name'];
+    $district = $_POST['district'];
+    $city = $_POST['city'];
+    $contact_number = $_POST['contact_number'];
+    $pincode = $_POST['pincode'];
+    $sql = "INSERT INTO Branches(BranchId,BranchName,District,City,ContactNum,Pin_code) VALUES('$branch_id','$branch_name','$district','$city','$contact_number','$pincode')";
+    mysqli_query($con,$sql);
 ?>
