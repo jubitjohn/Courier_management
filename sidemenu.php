a9dc79888918c6a0a04ca9b58fb56021d2af0cd8
Anushikapj <anushika1647@gmail.com>
2021-05-30 15:15:01 +0530

    sidemenu

--- sidemenu.php
+++ sidemenu.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Responsive Menubar</title>
-    <link rel="stylesheet" href="css\sidemenu.css">
+    <link rel="stylesheet" href="css/sidemenu.css">
 
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
