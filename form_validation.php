f19a6f27568f744853da56637b8aed2966fb0dec
Anushikapj <anushika1647@gmail.com>
2021-06-01 15:49:17 +0530

    changed

--- form_validation.php
+++ form_validation.php
@@ -3,15 +3,26 @@
 $nameErr="";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"])) {
+    if (empty($_POST["sender-name"])) {
       $nameErr = "Name is required";
     } else {
-      $name = test_input($_POST["name"]);
+      $name = test_input($_POST["sender-name"]);
       // check if name only contains letters and whitespace
       if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
         $nameErr = "Only letters and white space allowed";
       }
     }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      if (empty($_POST["receiver-name"])) {
+        $nameErr = "Name is required";
+      } else {
+        $name = test_input($_POST["receiver-name"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+          $nameErr = "Only letters and white space allowed";
+        }
+      } 
 
 if (isset ($POST['save']))
 {
