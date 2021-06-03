033a0a1ce93ae416623e1bf4cbedd462f0ef799d
Anushikapj <anushika1647@gmail.com>
2021-06-03 16:37:16 +0530

    formvalidation unsuccesful :(:(

--- form_validation.php
+++ form_validation.php
@@ -1,52 +1,51 @@
-<?php
-if (emptyempty ($_POST["sender-name"])) {  
-    $errMsg = "Error! You didn't enter the Name.";  
-             echo $errMsg;  
-} else {  
-    $name = $_POST["name"];  
-}  
 
-?>
-<?php
-$name = $_POST ["sender-name"];  
-if (!preg_match ("/^[a-zA-z]*$/", $name) ) {  
-    $ErrMsg = "Only alphabets and whitespace are allowed.";  
-             echo $ErrMsg;  
-} else {  
-    echo $name;  
-}  ?>
 
 <?php
-if (emptyempty ($_POST["receiver-name"])) {  
-    $errMsg = "Error! You didn't enter the Name.";  
-             echo $errMsg;  
-} else {  
-    $name = $_POST["name"];  
-}  
-
-?>
-<?php
-$name = $_POST ["receiver-name"];  
-if (!preg_match ("/^[a-zA-z]*$/", $name) ) {  
-    $ErrMsg = "Only alphabets and whitespace are allowed.";  
-             echo $ErrMsg;  
-} else {  
-    echo $name;  
-}  ?>
-
-<?php
-
-$error=""
-if (isset($_POST['Save'])) {
+$error="";
+if(isset($_POST['Save'])) {
     if(empty('sender-contact')){
 
-        $error = "Enter your mobile number "
+        $error = "Enter your mobile number ";
+        if (empty($_POST["sender-name"])) {  
+            $errMsg = "Error! You didn't enter the Name.";  
+                     echo $errMsg;  
+        } else {  
+            $name = $_POST["name"];  
+        }  
+        
+        ?>
+        <?php
+        $name = $_POST['sender-name'];  
+        if (!preg_match ("/^[a-zA-z]*$/", $name) ) {  
+            $ErrMsg = "Only alphabets and whitespace are allowed.";  
+                     echo $ErrMsg;  
+        } else {  
+            echo $name;  
+        }  ?>
+        
+        <?php
+        if (empty ($_POST["receiver-name"])) {  
+            $errMsg = "Error! You didn't enter the Name.";  
+                     echo $errMsg;  
+        } else {  
+            $name = $_POST["name"];  
+        }  
+        
+        ?>
+        <?php
+        $name = $_POST ["receiver-name"];  
+        if(!preg_match ("/^[a-zA-z]*$/", $name) ) {  
+            $ErrMsg = "Only alphabets and whitespace are allowed.";  
+                     echo $ErrMsg;  
+        } else {  
+            echo $name;  
+        }  
     } else if(strlen($_POST['sender-contact'])<10)
     {
-        $error = "Enter a valid mobile number "
+        $error = "Enter a valid mobile number ";
     }else if(!preg_match("/^[6-9]/d{9}$/",$_POST['sender-contact']))
     {
-        $error = "Enter a valid mobile number "
+        $error = "Enter a valid mobile number ";
     }
 }
 
@@ -54,17 +53,17 @@ if (isset($_POST['Save'])) {
 
 <?php
 
-$error=""
-if (isset($_POST['Save'])) {
+$error="";
+if(isset($_POST['Save'])) {
     if(empty('receiver-contact')){
 
-        $error = "Enter your mobile number "
+        $error = "Enter your mobile number ";
     } else if(strlen($_POST['receiver-contact'])<10)
     {
-        $error = "Enter a valid mobile number "
+        $error = "Enter a valid mobile number ";
     }else if(!preg_match("/^[6-9]/d{9}$/",$_POST['receiver-contact']))
     {
-        $error = "Enter a valid mobile number "
+        $error = "Enter a valid mobile number ";
     }
 }
 
@@ -72,19 +71,20 @@ if (isset($_POST['Save'])) {
 
 <?php
 
-$error=""
-if (isset($_POST['Save'])) {
+$error="";
+if(isset($_POST['Save'])) {
     if(empty('ref_no')){
 
-        $error = "Enter reference number "
+        $error = "Enter reference number ";
     } else if(strlen($_POST['ref_no'])<10)
     {
-        $error = "Enter a valid reference number "
+        $error = "Enter a valid reference number ";
     }else if(!preg_match("/^[6-9]/d{9}$/",$_POST['ref_no']))
     {
-        $error = "Enter a valid reference number "
+        $error = "Enter a valid reference number ";
     }
 }
 
 ?>
+
 
