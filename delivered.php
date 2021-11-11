ebe2939decd26d984183798f0605d57e5b2d2906
Anushikapj <anushika1647@gmail.com>
2021-11-11 21:47:46 +0530

    changes of home page

--- delivered.php
+++ delivered.php
@@ -60,7 +60,7 @@ $status = !empty($_GET['Status']) ? $_GET['Status'] : 'Intransit';
                         <?php
                         $i = 1;
 
-                        $qry = "SELECT * FROM parcel WHERE p_status = 'Intransit'";
+                        $qry = "SELECT * FROM parcel WHERE p_status = 'Order Received'";
                         $result = mysqli_query($conn, $qry);
                         if ($result) {
                             echo "Query Succesful";
